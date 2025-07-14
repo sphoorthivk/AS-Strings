c902ee255edcb65af04a4a9b9d06c68f64841bde
Nithyashri0804 <nithyashris2@gamil.com>
2025-07-14 15:02:00 +0530

    Z

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 👈 This makes assets load properly on Vercel
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
